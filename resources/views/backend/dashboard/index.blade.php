94c9c16e181ff1120112176cde47db9bfceba80f
example1 <32572574+bestshine@users.noreply.github.com>
2020-12-30 15:20:16 +0530

    update
    update

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.master')
 @section('title')
-    NagarikBazar Dashboard Page
+    Aquatech RO Dashboard Page
 @endsection
 @section('css')
     <link rel="stylesheet" href="{{asset('backend/plugins/select2.min.css')}}">
