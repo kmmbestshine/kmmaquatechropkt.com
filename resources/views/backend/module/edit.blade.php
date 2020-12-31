67b39d33567e8194c429dc7302c0ddc44ef7f850
example1 <32572574+bestshine@users.noreply.github.com>
2020-12-31 10:39:54 +0530

    update
    update

--- resources/views/backend/module/edit.blade.php
+++ resources/views/backend/module/edit.blade.php
@@ -39,7 +39,7 @@
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Create Module</h2>
+                            <h2>Update Module</h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
